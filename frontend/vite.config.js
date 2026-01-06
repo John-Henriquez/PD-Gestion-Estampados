b42c2557d19b709c791be9912b8b35677b814bd2
John-Henriquez <hjohnalexander@gmail.com>
2026-01-06 03:19:28 -0300

    hotfix n°1942834

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,35 +6,43 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default defineConfig({
-  plugins: [react()],
-  preview: { port: 443, host: true },
-  server: {
-    host: true,
-    strictPort: true,
-    allowedHosts: [
-      ".trycloudflare.com",
-      "localhost"
-    ],
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    preview: { 
+      port: 1624,
+      host: true,
+      strictPort: true,
+      proxy: {
+        '/api': {
+          target: 'http://127.0.0.1:80',
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-
-  resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, './src/components'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@context': path.resolve(__dirname, './src/context'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@services': path.resolve(__dirname, './src/services'),
-      '@styles': path.resolve(__dirname, './src/styles'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@helpers': path.resolve(__dirname, './src/helpers'),
+    server: {
+      port: 5173,
+      host: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+          secure: false,
+        },
+      },
     },
-  },
+    resolve: {
+      alias: {
+        '@components': path.resolve(__dirname, './src/components'),
+        '@hooks': path.resolve(__dirname, './src/hooks'),
+        '@context': path.resolve(__dirname, './src/context'),
+        '@pages': path.resolve(__dirname, './src/pages'),
+        '@services': path.resolve(__dirname, './src/services'),
+        '@styles': path.resolve(__dirname, './src/styles'),
+        '@assets': path.resolve(__dirname, './src/assets'),
+        '@helpers': path.resolve(__dirname, './src/helpers'),
+      },
+    },
+  };
 });
